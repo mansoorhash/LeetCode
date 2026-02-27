856b0777d27d7d6818aa831e88450d7c5cb5cdcf
Mansoor Hashemi <smansoorhash@gmail.com>
2026-02-27 17:31:39 -0500

    Time: 3 ms (80.44%) | Memory: 19.2 MB (79.82%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -2,32 +2,18 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         pair = {
             "I" : 1,
-            "IV": 4,
             "V" : 5,
-            "IX": 9,
             "X" : 10,
-            "XL": 40,
             "L" : 50,
-            "XC": 90,
             "C" : 100,
-            "CD": 400,
             "D" : 500,
-            "CM": 900,
             "M" : 1000
         }
         r = 0
-        skip = False
-        i = 0
-        while i < len(s):
-            if len(s) > i+1:
-                if s[i]+s[i+1] in pair:
-                    r+=pair[s[i]+s[i+1]]
-                    i += 2
-                    continue
-            if s[i] in pair:
-                r+=pair[s[i]]
-                i += 1
-
-
+        for i in range(len(s)):
+            if i + 1 < len(s) and pair[s[i]] < pair[s[i+1]]:
+                r -= pair[s[i]]
+            else:
+                r += pair[s[i]]
                 
         return r
