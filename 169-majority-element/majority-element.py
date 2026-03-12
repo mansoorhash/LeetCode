5730a9a1560a0845337e526ffd693ef3d857cbe5
Mansoor Hashemi <smansoorhash@gmail.com>
2026-03-12 02:00:05 -0400

    Time: 6 ms (61.64%) | Memory: 21.1 MB (97.24%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        f = {}
-        b = int()
+        count = 0
+        candidate = None
         for n in nums:
-            f[n] = f.get(n,0) + 1
-            if not b:
-                b = n
-            if n != b and f[n] > f[b]:
-                b = n
-        return b
+            if count == 0:
+                candidate = n
+            if n == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
