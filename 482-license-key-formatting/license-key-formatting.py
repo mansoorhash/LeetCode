28f208c6aeeffc63860e3ba3a06fe90b98bb77ac
Mansoor Hashemi <smansoorhash@gmail.com>
2026-07-19 12:53:09 -0400

    Time: 3 ms (93.25%) | Memory: 20 MB (45.19%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.py
+++ 482-license-key-formatting/license-key-formatting.py
@@ -1,15 +1,10 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        parts = s.split('-')
-        clean = ''.join(parts).upper()
-        r = len(clean) - 1
+        parts = s.replace("-","").upper()
+        r = len(parts)
         res = []
         while r > 0:
-            k_size = min(k, len(clean))
-            res.insert(0,clean[-k_size:])
-            
-            clean = clean[:-k_size]
-            r -= k_size
-        if clean:
-            res.insert(0, clean)
-        return "-".join(res)
+            l = max(0,r-k)
+            res.append(parts[l:r])
+            r = l
+        return "-".join(reversed(res))
