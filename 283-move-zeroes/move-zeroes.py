e2073a9c029c4330877d56194a547df1e978e42b
Mansoor Hashemi <smansoorhash@gmail.com>
2026-08-26 18:16:29 -0400

    Time: 3 ms (81.66%) | Memory: 20.4 MB (62.85%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,20 +6,10 @@ class Solution:
         if len(nums) == 1:
             return
         l = 0
-        r = 1
-        size_n = len(nums) -1
-        while l < size_n and l < r:
-            if not nums[l]:
-                while size_n > r and not nums[r]:
-                    r += 1
-                if r > size_n:
-                    pass
-                elif nums[r]:
-                    move_n = nums[r]
-                    nums[r] = nums[l]
-                    nums[l] = move_n
-            l += 1
-            r = l + 1
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
         
                 
 
