4821bcb8bf2ab3f411afcea739232ade478fee6b
Mansoor Hashemi <smansoorhash@gmail.com>
2026-08-28 13:28:15 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.48%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,8 +1,6 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if s and not t: return False
-        if not s and t: return True
-        if not s and not t: return True
+        if not s: return True
         s_point = 0
         t_point = 0
 
