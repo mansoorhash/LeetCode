3331b6def60cc89a2cc9f967f9436439e5dfd140
Mansoor Hashemi <smansoorhash@gmail.com>
2026-08-28 02:59:53 -0400

    Time: 15 ms (78.54%) | Memory: 19.6 MB (72.65%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,11 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         from collections import Counter
-        r_l = Counter(char.lower() for char in ransomNote)
-        m_l = Counter(char.lower() for char in magazine)
-
-        print(r_l)
-        print(m_l)
+        r_l = Counter(ransomNote)
+        m_l = Counter(magazine)
         
         for c, num in r_l.items():
             if c not in m_l:
