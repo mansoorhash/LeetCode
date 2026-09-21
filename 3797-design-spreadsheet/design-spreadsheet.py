127955a56253906a226c87c90f2905d8d265ad0a
Mansoor Hashemi <smansoorhash@gmail.com>
2026-09-21 17:45:22 -0400

    Time: 1419 ms (5.02%) | Memory: 27.2 MB (8.62%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.py
@@ -0,0 +1,28 @@
+class Spreadsheet:
+
+    def __init__(self, rows: int):
+        self.sheet = {
+            f"{chr(i)}{r+1}": 0
+            for r in range(rows)
+            for i in range(65, 91)
+        }
+        
+
+    def setCell(self, cell: str, value: int) -> None:
+        self.sheet[cell] = value
+
+    def resetCell(self, cell: str) -> None:
+        self.sheet[cell] = 0
+
+    def getValue(self, formula: str) -> int:
+        c1, c2 = formula.strip("=").split("+")
+        c1_value = int(c1) if c1.isdigit() else self.sheet[c1]
+        c2_value = int(c2) if c2.isdigit() else self.sheet[c2]
+        return c1_value + c2_value
+
+
+# Your Spreadsheet object will be instantiated and called as such:
+# obj = Spreadsheet(rows)
+# obj.setCell(cell,value)
+# obj.resetCell(cell)
+# param_3 = obj.getValue(formula)
